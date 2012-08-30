0d8fffa3d150547b69b00f7a906d776deb26ef9f
hmvp <github@hmvp.nl>
2012-08-30 20:12:08 +0200

    mysql

--- settings.py
+++ settings.py
@@ -11,11 +11,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/hiram/program-tracker/database.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'deb42924n2_progtrack',                      # Or path to database file if using sqlite3.
+        'USER': 'deb42924n2_pt',                      # Not used with sqlite3.
+        'PASSWORD': '488s$dg878ddd[f{6GX',                  # Not used with sqlite3.
+        'HOST': 's86.webhostingserver.nl',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
