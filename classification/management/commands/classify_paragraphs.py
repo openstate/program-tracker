de6d5d1da41869664e18377aa2abb46a8382c87f
Breyten Ernsting <bje@dds.nl>
2012-10-22 11:39:15 +0200

    Bare bones classification qirthout actual implementation.

--- classification/management/commands/classify_paragraphs.py
+++ classification/management/commands/classify_paragraphs.py
@@ -4,12 +4,29 @@ import datetime
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
-from core.models import Section, Program, Party
+from core.models import Section, Program, Party, Paragraph
 
 
 class Command(BaseCommand):
-    args = ''
-    help = 'Classifies the sections'
+    args = '<classifier name>'
+    help = 'Classifies the paragraph'
 
     def handle(self, *args, **options):
-        pass
+        classifier_name = args[0]
+        try:
+            classifier_class = classes.AVAILABLE_CLASSIFIERS[classifier_name]
+        except KeyError, e:
+            classifier_class = None
+
+        if classifier_class is None:
+            classifier_list = u','.join(classes.AVAILABLE_CLASSIFIERS)
+            print >>sys.stderr, "No classifier by such name found. Use one of : " + classifier_list
+            sys.exit(1)
+
+        classifier = classifier_class()
+
+        # FIXME: should find a way to only specify not classified paragraphs
+        paragraphs = Paragraph.objects.all()
+        
+        for paragraph in paragraphs:
+            classifier.classify(paragraph)
